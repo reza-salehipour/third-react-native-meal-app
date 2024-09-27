b862c63e7b7a6d619e0fec0d2ba41038f69e517f
Reza Sal <rez.salehipour@gmail.com>
2024-09-27 20:28:17 +0100

    finished the app without using redux or context api

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,18 +1,32 @@
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
 
 import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
-import { MEALS } from '../data/dummy-data';
+import {MEALS} from '../data/dummy-data';
+import {useLayoutEffect} from "react";
+import IconButton from "../components/iconButton";
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({route, navigation}) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
+    function headerButtonPressHandler() {
+        console.log("pressed")
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <IconButton icon="star" color="white" onPress={headerButtonPressHandler}/>
+            }
+        })
+    }, [navigation, headerButtonPressHandler]);
+
     return (
         <ScrollView style={styles.rootContainer}>
-            <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+            <Image style={styles.image} source={{uri: selectedMeal.imageUrl}}/>
             <Text style={styles.title}>{selectedMeal.title}</Text>
             <MealDetails
                 duration={selectedMeal.duration}
@@ -23,9 +37,9 @@ function MealDetailScreen({ route }) {
             <View style={styles.listOuterContainer}>
                 <View style={styles.listContainer}>
                     <Subtitle>Ingredients</Subtitle>
-                    <List data={selectedMeal.ingredients} />
+                    <List data={selectedMeal.ingredients}/>
                     <Subtitle>Steps</Subtitle>
-                    <List data={selectedMeal.steps} />
+                    <List data={selectedMeal.steps}/>
                 </View>
             </View>
         </ScrollView>
