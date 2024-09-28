aff926ad0fef367e0394d9b19b38e2c259e34924
Reza Sal <rez.salehipour@gmail.com>
2024-09-28 21:25:21 +0100

    finish using redux and this app

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavoritesScreen from './screens/FavoriteScreen';
-import FavoritesContextProvider from './store/context/favorites-context';
+// import FavoritesContextProvider from './store/context/favorites-context';
+import {Provider} from "react-redux";
+import {store} from "./store/redux/sore";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -53,7 +55,8 @@ export default function App() {
     return (
         <>
             <StatusBar style="light" />
-            <FavoritesContextProvider>
+            {/*<FavoritesContextProvider>*/}
+            <Provider store={store}>
                 <NavigationContainer>
                     <Stack.Navigator
                         screenOptions={{
@@ -82,7 +85,8 @@ export default function App() {
                         />
                     </Stack.Navigator>
                 </NavigationContainer>
-            </FavoritesContextProvider>
+            </Provider>
+            {/*</FavoritesContextProvider>*/}
         </>
     );
 }
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -4,12 +4,17 @@ import { useContext } from 'react';
 import MealsList from '../components/MealsList/MealsList';
 import { FavoritesContext } from '../store/context/favorites-context';
 import { MEALS } from '../data/dummy-data';
+import {useSelector} from "react-redux";
 
 function FavoritesScreen() {
-    const favoriteMealsCtx = useContext(FavoritesContext);
+    // const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
+    // const favoriteMeals = MEALS.filter((meal) =>
+    //     favoriteMealsCtx.ids.includes(meal.id)
+    // );    
     const favoriteMeals = MEALS.filter((meal) =>
-        favoriteMealsCtx.ids.includes(meal.id)
+        favoriteMealIds.ids.includes(meal.id)
     );
 
     if (favoriteMeals.length === 0) {
